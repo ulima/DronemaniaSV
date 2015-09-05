f09dc382ff53abae31c3b8850a61c8bb36ca83a3
hernan <hquintana@devosinc.com>
2015-09-05 12:50:32 -0500

    Se termino el controlador (servlet) incluyendo la configuracion en el web.xml. Tambien se agrego la libreria gson (para parseo de json) al proyecto.

--- src/java/pe/edu/ulima/controllers/LoginServlet.java
+++ src/java/pe/edu/ulima/controllers/LoginServlet.java
@@ -1,10 +1,19 @@
 package pe.edu.ulima.controllers;
 
+import com.google.gson.Gson;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.io.Reader;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import pe.edu.ulima.controllers.dto.LoginRequest;
+import pe.edu.ulima.controllers.dto.LoginResponse;
+import pe.edu.ulima.model.GestorUsuarios;
+import pe.edu.ulima.utilitarios.Utiles;
 
 /*
  * Input:
@@ -14,8 +23,8 @@ import javax.servlet.http.HttpServletResponse;
  * }
  * Output:
  * {
- *   "msg_status": "OK",
- *   "msg_error" : ""
+ *   "msgStatus": "OK",
+ *   "msgError" : ""
  * }
 */
 
@@ -23,7 +32,31 @@ public class LoginServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-
+        InputStream is = request.getInputStream();
+        
+        Reader reader = new InputStreamReader(is, "UTF-8");
+        LoginRequest loginRequest = 
+                new Gson().fromJson(reader, LoginRequest.class);
+        
+        GestorUsuarios gestorUsuarios = new GestorUsuarios();
+        boolean res = gestorUsuarios.login(
+                loginRequest.getUsuario(), 
+                loginRequest.getPassword()
+        );
+        
+        String json = "";
+        if (res){
+            LoginResponse loginResponse = new LoginResponse("OK", "");
+            json = new Gson().toJson(loginResponse);
+        }else{
+            LoginResponse loginResponse = new LoginResponse("Error", 
+                    "Login incorrecto");
+            json = new Gson().toJson(loginResponse);
+        }
+        response.setContentType("application/json");
+        PrintWriter out = response.getWriter();
+        out.print(json);
+        
     }
 
 }
